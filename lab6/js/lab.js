fbd0e448fa9bc4f141d3c090a035638e088351f9
nmakranyi <nmakrany@ucsc.edu>
2024-10-23 08:00:30 -0700

    Worked on lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,20 +1,49 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author: Nikolas Makranyi <nmakrany@ucsc.edu>
+ * Created: 10.22.2024
+ * Purpose: Lab 6 JavaScript file, declaring arrays and objects.
+ * License: Public Domain
+ **/
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
 function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+  // declaring array and object
+  let myTransport = ["Car", "Bus", "Bike", "Foot", "Skateboard"];
+  const myMainRide = {
+    make: "Lamborghini",
+    model: "Aventador",
+    color: "Blue",
+    year: 2022,
+    age: function() {
+        return 2024 - this.year;
+    }
+  };
+
+  // custom object
+  const myPet = {
+    name: "Simba",
+    species: "Cat",
+    age: 7,
+    color: "Orange",
+    indoor: true
+  };
+
+  // printing
+  document.writeln("Getting around: " + myTransport + "<br>");
+  document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
+  document.writeln("<br>");
+  document.writeln("Looping through myTransport:<br>");
+  for (let index = 0; index < myTransport.length; index++)
+  {
+    document.writeln(myTransport[index] + "<br>");
+  }
+  document.writeln("<br>");
+  document.writeln("Printing custom object called myPet: <br>");
+  document.writeln("My Pet: <pre>", JSON.stringify(myPet, null, '\t'), "</pre>");
+  document.writeln("<br>");
 }
 
 // let's get this party started
