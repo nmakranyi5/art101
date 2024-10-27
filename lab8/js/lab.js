e80e74c5ee931430a0d32359ebee144b1c5bb73d
nmakranyi <nmakrany@ucsc.edu>
2024-10-27 15:15:07 -0700

    Worked on lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,92 +1,43 @@
 /**
  * Author: Nikolas Makranyi <nmakrany@ucsc.edu>
- * Created: 10.24.2024
- * Purpose: Lab 7 JavaScript file, using functions.
+ * Created: 10.27.2024
+ * Purpose: Lab 8 JavaScript file, using anon functions and callbacks.
  * License: Public Domain
  **/
 
 // Constants
 
 // Functions
-function sortNameNoSpace(name)
-{
-  // convert string to lowercase
-  let userName = name.toLowerCase();
-  // splitting the string
-  let words = userName.split("");
-  // sorting the words
-  words.sort();
-  // rejoining the array elements to a string
-  let sortedName = words.join("");
-  // returning string with sorted words
-  return sortedName;
-}
-
-function sortName(name)
-{
-  // convert string to lowercase
-  let userName = name
-  // splitting the string
-  let words = userName.split(" ");
-  // sorting the words
-  words.sort();
-  // rejoining the array elements to a string
-  let sortedName = words.join(" ");
-  // returning string with sorted words
-  return sortedName;
-}
-
-// Functions
-function reverseName(name)
-{
-  // splitting the string
-  let words = name.split("");
-  // sorting the words
-  words.reverse();
-  // rejoining the array elements to a string
-  let sortedName = words.join("");
-  // returning string with sorted words
-  return sortedName;
-}
-
-function anagram(name)
-{
-  // convert string to lowercase
-  let userName = name.toLowerCase();
-  // splitting the string  
-  let words = userName.split("");
-  // sorting the words, got this line from W3Schools
-  words.sort(() => 0.5 - Math.random());
-  // looping through and capitalizing first letter of words
-  words[0] = words[0].toUpperCase();
-  for(let index = 1; index < words.length; index++)
-  {
-    if(words[index - 1] == " ")
-    {
-      words[index] = words[index].toUpperCase();
-    }
-  }
-  // rejoining the array elements to a string
-  let sortedName = words.join("");
-  // returning string with sorted words
-  return sortedName;
+// square root function
+function squareRoot(x){
+  return Math.sqrt(x);
 }
 
 function main() {
-  // getting user input
-  let name = window.prompt("Enter your name here:");
-  // printing stuff and calling functions
-  document.writeln("Your name: " + name + "<br>");
-  let sortedNameNoS = sortNameNoSpace(name);
-  document.writeln("Your sorted name without spaces: " + sortedNameNoS + "<br>");
-  let sortedName = sortName(name);
-  document.writeln("Your sorted name with spaces: " + sortedName + "<br>");
-  let revName = reverseName(name);
-  document.writeln("Your reversed name: " + revName + "<br>");
-  let anagramName = anagram(name);
-  document.writeln("Your name as an anagram: " + anagramName + "<br>");
-  // putting anagram on name tag div
-  document.getElementById("nametagText").innerHTML = anagramName;
+  // creating array of numbers
+  let nums = [100, 238402349, 993, 23949, 0, 2.4, 55, 1400, 300, 99];
+
+  // testing the sqrt function
+  console.log("Testing squareRoot function: ", squareRoot(nums[0]));
+  console.log("Testing squareRoot function: ", squareRoot(nums[8]));
+  console.log("Testing squareRoot function: ", squareRoot(nums[3]));
+
+  // looping through the array and calling function on each element
+  for(let index = 0; index < nums.length; index++)
+  {
+    console.log("Looping through nums: ", squareRoot(nums[index]));
+  }
+
+  // calling square root function and printing output to console
+  var results = nums.map(squareRoot);
+  console.log("Squareroot of nums: ", results);
+
+  // anon function with callback that multiplies each array element by 2
+  var results = nums.map(function(x){
+    return x * 2;
+  })
+  // print array elements * 2
+  console.log("Nums * 2: ", results);
 }
 
 // let's get this party started
