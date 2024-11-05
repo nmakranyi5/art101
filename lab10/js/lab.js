00e423338bd1ee227b7f7135ca2d48dcc3545ae8
nmakranyi <nmakrany@ucsc.edu>
2024-11-05 13:36:14 -0800

    Worked on lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,46 +1,34 @@
 /**
  * Author: Nikolas Makranyi <nmakrany@ucsc.edu>
- * Created: 10.29.2024
- * Purpose: Lab 9 JavaScript file, using libraries and jQuery.
+ * Created: 11.4.2024
+ * Purpose: Lab 10 JavaScript file, appends new element to an output div.
  * License: Public Domain
+ * Requirements: jQuery must be loaded for this script to work.
  **/
 
 // Constants
 
 // Functions
+function generateRandomText() 
+{
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  const min = 3;
+  const max = 100;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
 
-function main() {
-  /* old code
-  // add button to challenge section
-  $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-  // add button to problems section
-  $("#problems").append("<button id='button-problems'>Make Special</button>");
-  // add button to reflection section
-  $("#reflection").append("<button id='button-reflection'>Make Special</button>");
-
-  // add a click listener to the challenge button
-  $("#button-challenge").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#challenge").toggleClass("special");
-  });
-  // add a click listener to the challenge button
-  $("#button-problems").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#problems").toggleClass("special");
-  });
-  // add a click listener to the challenge button
-  $("#button-reflection").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $("#reflection").toggleClass("special");
-  });
-  */
-  // new code
-  // add button to all three sections
-  $(".special_section").append("<button class='special-sections-button'>Make Special</button>");
-  // add a click listener to the button
-  $(".special-sections-button").click(function(){
-    // now add (or subtract) the "special" class to the section
-    $(this).parent().toggleClass("special");
+function main() 
+{
+  // click listener for button
+  $("#make-convo").click(function(){
+    // get new fake dialogue
+    const newText = generateRandomText();
+    // append a new div to our output div
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
   });
 }
 
