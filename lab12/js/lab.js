75a95d1e5c4fea0f17a33968de4720a6eb1b1ab8
nmakranyi <nmakrany@ucsc.edu>
2024-11-12 21:02:58 -0800

    finished lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -7,39 +7,37 @@
  **/
 
 // Constants
-const houses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"]
+const houses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff", "Canisfang"]
+const descriptions = ["Gryffindor is Harry Potter's house, and is known for having valiant and brave members.", "Ravenclaw is known for the intelligence and wisdom of its members.", "Slytherin is known for having members who are cunning, and willing to put themselves first.", "Hufflepuff's members are known for being kind and generous, and always willing to stand for what they believe in.", "Canisfang is a new house, with the symbol of a wolf, and whose members are known for camaraderie and willingness to act on each other's behalf."]
+const images = ["./img/gryffindor.jpeg", "./img/ravenclaw.jpg", "./img/slytherin.png", "./img/hufflepuff.jpg", "./img/canisfang.jpg"];
 // Functions
-function sortingHat(input)
-{
-  let length = input.length;
-  let mod = length % 4;
-  if(mod === 0)
-  {
-    return "Gryffindor";
+function hashCode(str) { // got this code from the Stack Overflow linked by the professor.
+  let hash = 0;
+  for (let i = 0, len = str.length; i < len; i++) {
+      let chr = str.charCodeAt(i);
+      hash = (hash << 5) - hash + chr;
+      hash |= 0; // Convert to 32bit integer
   }
-  else if(mod === 1)
-  {
-    return "Ravenclaw";
-  }
-  else if(mod === 2)
-  {
-    return "Slytherin";
-  }
-  else if(mod === 3)
-  {
-    return "Hufflepuff";
-  }
+  return hash;
 }
 
+function sortingHat(input)
+{
+  let hash = Math.abs(hashCode(input) % houses.length); // calculate the hash using the helper function and modulo
+  return hash;
+}
 
 function main() 
 {
   let myButton = document.getElementById("button");
   myButton.addEventListener("click", function() {
     let name = document.getElementById("input").value;
-    let house = sortingHat(name);
+    let hash = sortingHat(name);
+    let house = houses[hash];
+    let description = descriptions[hash];
     let newString = "<p>The Sorting Hat has sorted you into " + house + "</p>";
-    document.getElementById("output").innerHTML = newString;
+    document.getElementById("output").innerHTML = newString + description;
+    document.getElementById("photo").innerHTML = `<img src="${images[hash]}" style="width:200px;height:auto;">`;
   })
 }
 
