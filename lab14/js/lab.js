491cbee0e1d77911ceb0ab2e200e6c1c53f9caca
nmakranyi <nmakrany@ucsc.edu>
2024-11-17 17:45:38 -0800

    worked on lab 14

---
+++ lab14/js/lab.js
@@ -0,0 +1,20 @@
+/**
+ * Author: Nikolas Makranyi <nmakrany@ucsc.edu>
+ * Created: 11.17.2024
+ * Purpose: Lab 14 JavaScript file, testing out the debugger here.
+ * License: Public Domain
+ * Requirements: jQuery must be loaded for this script to work.
+ **/
+
+// Constants
+// Functions
+function main() 
+{
+    let num = 1;
+    num += 10 + 1238123 * 50;
+    num = num + num;
+    return num;
+}
+
+// let's get this party started
+main();
