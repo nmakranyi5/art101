a36fab8a4db1f357c65972aaedc3f422a6def3c7
nmakranyi <nmakrany@ucsc.edu>
2024-11-22 22:14:35 -0800

    Added NASA API

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -33,6 +33,38 @@ function main()
             }
         })
     })
+    $.ajax({
+        // The URL for the request (from the api docs)
+        url: "https://api.nasa.gov/planetary/apod",
+        // Whether this is a POST or GET request
+        type: "GET",
+        // The data to send (will be converted to a query string)
+        data: { 
+            // here is where any data required by the api 
+            //   goes (check the api docs)
+            api_key: "ILQAuEhXrL9zlRh4UUmlsuE5HZmIWCbOFMX3sLzk",
+          },
+        // The type of data we expect back
+        dataType : "json",
+        // What do we do when the api call is successful
+        // all the action goes in here
+        success: function(data) {
+            // do stuff
+            const imageUrl = data.hdurl;
+            const explanation = data.explanation;
+            $("#nasaPhoto").html(`
+                <img src="${imageUrl}" style="max-width: 700px; height: 600px;"/>
+            `);
+            $("#photoText").html(`
+                <p>${explanation}</p>
+            `);
+        },
+        // What we do if the api call fails
+        error: function (jqXHR, textStatus, errorThrown) { 
+            // do stuff
+            console.log("Error:", textStatus, errorThrown);
+        }
+    })
 }
 
 // let's get this party started
